a3a1c3803be194993e1aa11f73317d5ea7406a70
Ashish <ashish.saxena25@gmail.com>
2020-12-12 17:10:12 -0500

    Variable Practice_ Primitive number example

---
+++ src/com/company/Main.java
@@ -0,0 +1,128 @@
+package com.company;
+
+import sun.font.TrueTypeFont;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int population = 100000;
+        int size = 24;
+        short meetingDuration = 2;
+        short time = 24;
+        long bignumber = 983456789124567893L;
+        long metteRReading = 7894556122222L;
+        System.out.println(population);
+        System.out.println(bignumber);
+        System.out.println(bignumber);
+        System.out.println(size);
+        System.out.println(meetingDuration);
+        System.out.println(time);
+        System.out.println(metteRReading);
+        double amount = 30000.1234587694;
+        float balance = 400.7374938474729347f;
+        double amount2 = 9865.5723547234527454737;
+        float balance6 = 4684.78463784635634863468472f;
+        System.out.println(amount);
+        System.out.println(balance);
+        System.out.println(amount2);
+        System.out.println(balance6);
+        int totalHotels = 567;
+        short roomInHotel = 64;
+        long workingStaff = 1232326449995656L;
+        double monthlyTurnover = 2565.6465656565656565555555555555555555;
+        float yearlyTurnover = 5465.464649646562646949464161646946461616f;
+        System.out.println(totalHotels);
+        System.out.println(roomInHotel);
+        System.out.println(workingStaff);
+        System.out.println(monthlyTurnover);
+        System.out.println(yearlyTurnover);
+        boolean isCanadian = false;
+        System.out.println(isCanadian);
+        boolean isYearEnding = true;
+        System.out.println(isYearEnding);
+        //
+
+        float startInSky = 459796.596798978565989f;
+        System.out.println(startInSky);
+        double starInSky1 = 459796.5979897;
+        System.out.println(starInSky1);
+
+        //
+        int num1 = 20;
+        int num2 = 30;
+        int sum = num1+num2;
+        System.out.println(sum);
+        System.out.println((20-30));
+        System.out.println(20/30);
+        System.out.println(20*30);
+        System.out.println(20%30);
+
+        // declare 2 number of type of double and apply all arithmetic operations and print the result
+
+        double num3 ,num4;
+        num3 = 10.50;
+        num4 = 20.25;
+
+        double result ;
+
+        result = num3+num4;
+        System.out.println(result);
+
+        result = num3-num4;
+        System.out.println(result);
+
+        result = num3*num4;
+        System.out.println(result);
+
+        result = num3/num4;
+        System.out.println(result);
+
+        result = num3%num4;
+        System.out.println(result);
+
+        // apply relational operator on 2 intrger types variables and print the outcome.
+
+        System.out.println(num1 > num2);
+        System.out.println(num1 < num2);
+        System.out.println(num3 >= num4);
+        System.out.println(num1<=num2);
+
+        //
+
+        System.out.println(1 > 2);
+        System.out.println(1 < 2);
+        System.out.println(3 >= 4);
+        System.out.println(1<=2);
+
+        // write a logic to print number
+
+
+        public static double {
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    }
+
